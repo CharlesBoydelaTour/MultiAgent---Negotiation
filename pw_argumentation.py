b7d16740832fc1e27ef33a5a0f39dfd81b61c7c2
CharlesBoydelaTour <charles.boydelatour@gmail.com>
2022-03-25 10:00:29 +0100

    add generate_preference (random preference)

--- pw_argumentation.py
+++ pw_argumentation.py
@@ -5,6 +5,8 @@ import numpy as np
 from communication.agent.CommunicatingAgent import CommunicatingAgent
 from communication.message.MessageService import MessageService
 from communication.preferences.CriterionName import CriterionName
+from communication.preferences.Preferences import Preferences
+from communication.preferences.CriterionValue import CriterionValue
 
 class ArgumentAgent(CommunicatingAgent):
     """ ArgumentAgent which inherit from CommunicatingAgent."""
@@ -20,8 +22,25 @@ class ArgumentAgent(CommunicatingAgent):
     
     def generate_preference(self, list_of_items):
         """give a score for each item for each agent""" 
-        
+        self.preference = Preferences()
+        self.preference.set_criterion_name_list([CriterionName.PRODUCTION_COST, CriterionName.ENVIRONMENT_IMPACT,
+                                        CriterionName.CONSUMPTION, CriterionName.DURABILITY,
+                                        CriterionName.NOISE])
         for item in list_of_items:
-            for criter in range(5):
-                value = np.random.choice([0,1,2,3])
-                agent_pref.add_criterion_value(CriterionValue(item,CriterionName.criter, CriterionValue.value))
+            self.preference.add_criterion_value(CriterionValue(item, CriterionName.PRODUCTION_COST,
+                                                          np.random.randint(0, 5)))
+            self.preference.add_criterion_value(CriterionValue(item, CriterionName.CONSUMPTION,
+                                                          np.random.randint(0, 5)))
+            self.preference.add_criterion_value(CriterionValue(item, CriterionName.DURABILITY,
+                                                          np.random.randint(0, 5)))
+            self.preference.add_criterion_value(CriterionValue(item, CriterionName.ENVIRONMENT_IMPACT,
+                                                          np.random.randint(0, 5)))
+            self.preference.add_criterion_value(CriterionValue(item, CriterionName.NOISE,
+                                                          np.random.randint(0, 5)))
+    
+if __name__ == '__main__':
+    """Testing the ArgumentAgent class.
+    """
+    agent_pref = ArgumentAgent(1, None, "agent")
+    agent_pref.generate_preference(["item1", "item2"])
+    print(agent_pref.get_preference().get_criterion_name_list())
